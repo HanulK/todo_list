cead23e083e98be2c5a23993c6ccfe61054b57da
HanulK <scnnee@hanmail.net>
2023-09-09 20:39:59 +0900

    initialize

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* App 컴포넌트 불러오기 */}
-    <App />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root') //id값이 root인 태그 랜더링
 );
-reportWebVitals();
