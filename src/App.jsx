e2d54ae559fd0b028793c8a64438ad11c08d3f93
HanulK <scnnee@hanmail.net>
2023-09-09 20:21:17 +0900

    Using Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-// import '.App.css';
+import './App.css';
+import Box from "./Box";
 
 function App() {
     return (
         <main>
-            React + Git
+            <Box/>
+            <Box/>
+            <Box/>
+            
+            {/* <div className="box">
+                Box1
+                Rabbit
+            </div>
+
+            <div className="box">
+                Box2
+                Rabbit
+            </div> */}
         </main>
     );
 }
---
+++ src/Box.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Box() {
+    return (
+        <div className="box">
+            content
+        </div>
+    );
+}
+
+export default Box;
