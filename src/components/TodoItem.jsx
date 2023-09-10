19fb9242cc30020266730ba74262a9da5624b69b
HanulK <scnnee@hanmail.net>
2023-09-10 17:37:34 +0900

    삭제 확인 과정 추가

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,11 +4,17 @@ import cn from "classnames";
 function TodoItem({todo, onRemove, onToggle}) {
     const { id, text, checked } = todo;
 
+    const checkRM = (id) => {
+        if(window.confirm('삭제 하시겠습니까?')){
+            onRemove(id)
+        }
+    }
+
     return(
         <div className="todo-item">
             <div className={cn('check_circle', {off: checked})} onClick={() => onToggle(id)}></div>
             <div className={cn('content', {off: checked})}>{text}</div>
-            <div className="remove" onClick={() => onRemove(id)}></div>
+            <div className="remove" onClick={() => checkRM(id)}></div>
         </div>
     );
 }
